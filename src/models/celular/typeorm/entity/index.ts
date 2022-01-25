0107d4643a20241586db9b8b194bf485fe88b98f
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-01-25 19:10:29 -0300

    Feita a criacao da entidade de celulares

---
+++ src/models/celular/typeorm/entity/index.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("celulares")
+export class Celular {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "string", nullable: true })
+  imgName: string;
+
+  @Column({ type: "string", nullable: true })
+  imgPath: string;
+
+  @Column("string")
+  nome: string;
+
+  @Column("string")
+  modelo: string;
+
+  @Column("string")
+  imeis: string[];
+
+  @CreateDateColumn({ type: "timestamp with time zone" })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: "timestamp with time zone" })
+  updated_at: Date;
+}
