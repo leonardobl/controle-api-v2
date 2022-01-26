c6930a70fb5addc8c4bd2d6dc17ff95cf685b52a
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-01-26 08:57:39 -0300

    merge com colaboradores

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,7 @@
 import { Router } from "express";
 
-<<<<<<< HEAD
-import ColaboradoresRoutes from "../models/colaboradores/routes/Colaboradores.routes";
-=======
 import { ColaboradoresRoutes } from "../models/colaboradores/routes/Colaboradores.routes";
 import { NotebooksRoutes } from "../models/notebooks/routers/Notebooks.routes";
->>>>>>> master
 
 export const Routes = Router();
 
