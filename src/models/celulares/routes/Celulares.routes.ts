6f726fd36f0837d4440d03dd382e78f51c28a676
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-01-26 10:05:25 -0300

    criaçao do config do celebrate celulares

--- src/models/celulares/routes/Celulares.routes.ts
+++ src/models/celulares/routes/Celulares.routes.ts
@@ -5,4 +5,5 @@ import CelularesControllers from "../controller";
 export const CelularesRoutes = express.Router();
 
 CelularesRoutes.get("/", CelularesControllers.index);
+
 CelularesRoutes.post("/", CelularesControllers.create);
