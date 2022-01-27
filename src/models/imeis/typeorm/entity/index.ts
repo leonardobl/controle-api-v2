6c9f9353bb388cce87d9c817c3fa19e413e7eeb0
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-01-27 07:29:33 -0300

    criaçao da entidade de imeis

---
+++ src/models/imeis/typeorm/entity/index.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("imeis")
+class Imeis {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column("varchar")
+  imei1: string;
+
+  @Column("varchar")
+  imei2: string;
+
+  @CreateDateColumn({ type: "timestamp with time zone" })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: "timestamp with time zone" })
+  updated_at: Date;
+}
+
+export default Imeis;
