1e6ee230da93d51d1ed0296f9f417a05c35f6c52
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-01-27 08:18:32 -0300

    service create finalizado

--- src/models/imeis/controllers/index.ts
+++ src/models/imeis/controllers/index.ts
@@ -5,5 +5,8 @@ import CreateImeisService from "../services/CreateImeisService";
 class ImeisControllers {
   public async create(req: Request, res: Response): Promise<Response> {
     const imei = await CreateImeisService.execute(req);
+    return res.status(201).json(imei);
   }
 }
+
+export default new ImeisControllers();
