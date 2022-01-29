3226684996fcbd94018ad52916ddbbb4ab0f17b4
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-01-29 08:52:20 -0300

    Criacao da rota update notebook

--- src/models/notebooks/routers/Notebooks.routes.ts
+++ src/models/notebooks/routers/Notebooks.routes.ts
@@ -1,10 +1,19 @@
+import { celebrate } from "celebrate";
 import express from "express";
 
+import celebrateConfig from "../configs/celebratePostConfig";
 import NotebooksController from "../controller";
 
 const NotebooksRoutes = express.Router();
 
 NotebooksRoutes.get("/", NotebooksController.index);
-NotebooksRoutes.post("/", NotebooksController.create);
+
+NotebooksRoutes.post(
+  "/",
+  celebrate(celebrateConfig),
+  NotebooksController.create
+);
+
+NotebooksRoutes.put("/:id", NotebooksController.update);
 
 export default NotebooksRoutes;
