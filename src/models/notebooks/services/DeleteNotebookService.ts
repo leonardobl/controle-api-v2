d4f59f942dd61a1076637288b2418ad7d3874d9a
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-01-29 09:42:26 -0300

    criacao do service de delete

---
+++ src/models/notebooks/services/DeleteNotebookService.ts
@@ -0,0 +1,17 @@
+import { getCustomRepository } from "typeorm";
+
+import AppError from "../../../shared/errors/AppError";
+import NotebooksCustomRepository from "../typeorm/repository";
+
+class DeleteUpdateService {
+  public async execute(id: string) {
+    const notebookRepository = getCustomRepository(NotebooksCustomRepository);
+    const notebook = await notebookRepository.findOne({ where: { id } });
+
+    if (!notebook) throw new AppError("Notebook not found");
+
+    await notebookRepository.remove(notebook);
+  }
+}
+
+export default new DeleteUpdateService();
