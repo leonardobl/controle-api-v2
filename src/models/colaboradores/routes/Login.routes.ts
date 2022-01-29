790de3999b1edabbe7c48902daa37989ad96b7f7
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-01-29 18:41:29 -0300

    validacao username/senha

--- src/models/colaboradores/routes/Login.routes.ts
+++ src/models/colaboradores/routes/Login.routes.ts
@@ -1,9 +1,19 @@
+import { celebrate, Joi, Segments } from "celebrate";
 import { Router } from "express";
 
 import LoginController from "../controller/LoginController";
 
 const LoginRoutes = Router();
 
-LoginRoutes.post("/", LoginController.create);
+LoginRoutes.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      username: Joi.string().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  LoginController.create
+);
 
 export default LoginRoutes;
